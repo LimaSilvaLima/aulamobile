b09b33dbb19a60316e65586b1eb13aca696372fd
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-03-13 11:46:36 -0300

    projeto instalado

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: '#0f172a',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+//
