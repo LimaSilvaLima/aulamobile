0ab13f118d842d1d205e67b0cd3c4562fc510cb0
Jose Silva Lima <limasilvalima@hotmail.com>
2026-04-28 19:15:26 -0300

    Publicada tela inicial com HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={'light-content'} />
+      <HomeScreen />
+    </>
+      
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,32 @@
 import { View, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.title}>React Native</Text>
+      <Text style={styles.subtitle}>Projeto Mobile com React Native TypeScript</Text>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({      
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: "#0f172a",
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    title: {
+      fontSize: 42,
+      fontWeight: '800',
+      color: '#38bdf8',
+    },
+    subtitle: {
+    marginTop: 10,
+      fontSize: 16,
+      fontWeight: '600',
+      color: '#e5e7eb',
+      opacity: 0.8,
     },
   });
