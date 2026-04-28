8090047d123bed72a8300c46d3c3f55fc3d998b2
Jose Silva Lima <limasilvalima@hotmail.com>
2026-04-28 15:50:46 -0300

    projeto re-instalado

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from "react-native";
-import HomeScreen from "./src/screens/HomeScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle={"light-content"} />
-      //<HomeScreen />
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-//
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
